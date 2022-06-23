fcd4caf9580cc7daa1fdb2bf2b799b29bc188c73
Jacob Jewell <jacobjewell@eflexsystems.com>
2022-06-23 15:20:44 -0400

    remove apollo

--- addon/index.js
+++ addon/index.js
@@ -1,2 +1,1 @@
 export { default as handleUnauthorized } from "ember-simple-auth-oidc/unauthorized";
-export { default as apolloMiddleware } from "ember-simple-auth-oidc/apollo";
--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -6,9 +6,6 @@ module.exports = function (environment) {
     environment,
     rootURL: "/",
     locationType: "history",
-    apollo: {
-      apiURL: "http://localhost:4200/graphql",
-    },
     "ember-simple-auth-oidc": {
       host: "http://localhost:4200/realms/test-realm",
       clientId: "test-client",
